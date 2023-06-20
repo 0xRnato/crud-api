473f2faae07f52edd4a1611e251b954cd2259823
Renato Neto <rnato.netoo@gmail.com>
2023-06-20 10:10:55 -0300

    feat: validation pipe

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,11 @@
+import { IsDefined, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsDefined()
+  @IsString()
   name: string;
+
+  @IsDefined()
+  @IsString()
   job: string;
 }
---
+++ src/util/validation/validation.pipe.ts
@@ -0,0 +1,42 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+import { validate } from 'class-validator';
+import { plainToClass } from 'class-transformer';
+
+@Injectable()
+export class ValidationPipe implements PipeTransform {
+  constructor(private readonly minimumAge?: number) {}
+
+  async transform(value: any, { metatype }: ArgumentMetadata) {
+    if (!metatype || !this.toValidate(metatype)) {
+      return value;
+    }
+
+    const object = plainToClass(metatype, value);
+    const errors = await validate(object);
+
+    if (errors.length > 0) {
+      const errorMessages = errors.reduce((messages, error) => {
+        for (const key in error.constraints) {
+          messages.push(error.constraints[key]);
+        }
+        return messages;
+      }, []);
+      throw new BadRequestException({
+        success: false,
+        errors: errorMessages,
+      });
+    }
+
+    return value;
+  }
+
+  private toValidate(metatype: any): boolean {
+    const types = [String, Boolean, Number, Array, Object];
+    return !types.includes(metatype);
+  }
+}
